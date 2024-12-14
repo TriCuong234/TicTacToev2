887b71034ad86232c4d65ba46506caa1f5f47b66
TriCuong234 <cuongtri2324@gmail.com>
2024-12-14 20:47:38 +0700

    Init Layout

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField]
+    private GameObject PvPbtn;
+
+    [SerializeField]
+    private GameObject PvEbtn;
+    [SerializeField]
+    private GameObject Exitbtn;
+
     void Start()
     {
-        
+        PvPbtn.GetComponent<Button>().onClick.AddListener(OnClickPvP);
+        PvEbtn.GetComponent<Button>().onClick.AddListener(OnClickPvE);
+        Exitbtn.GetComponent<Button>().onClick.AddListener(OnClickExit);
     }
 
     // Update is called once per frame
-    void Update()
+
+    void OnClickPvP()
     {
-        
+        print("hehe");
+    }
+    void OnClickPvE()
+    {
+        print("hehe");
+    }
+    void OnClickExit()
+    {
+        Application.Quit();
+        print("Dang Quitting");
     }
 }
